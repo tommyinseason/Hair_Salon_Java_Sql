9d894922458daeceff7ed97d3270ac147b52df3e
Tommy Jones <tommyinseason@yahoo.com>
2016-05-11 16:24:11 -0700

    Updated dbrule page

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -1,18 +1,18 @@
-// import org.junit.rules.ExternalResource;
-// import org.sql2o.*;
-//
-// public class DatabaseRule extends ExternalResource {
-//
-//   protected void before() {
-//     DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", null, null);
+import org.junit.rules.ExternalResource;
+import org.sql2o.*;
 
-//
-//   protected void after() {
-//     try(Connection con = DB.sql2o.open()) {
-//       String deleteRestaurantsQuery = "DELETE FROM restaurants *;";
-//       String deleteCuisineQuery = "DELETE FROM cuisine *;";
-//       con.createQuery(deleteRestaurantsQuery).executeUpdate();
-//       con.createQuery(deleteCuisineQuery).executeUpdate();
-//     }
-//   }
-// }
+public class DatabaseRule extends ExternalResource {
+
+  protected void before() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", null, null);
+
+
+  protected void after() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteRestaurantsQuery = "DELETE FROM restaurants *;";
+      String deleteCuisineQuery = "DELETE FROM cuisine *;";
+      con.createQuery(deleteRestaurantsQuery).executeUpdate();
+      con.createQuery(deleteCuisineQuery).executeUpdate();
+    }
+  }
+}
